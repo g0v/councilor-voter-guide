42362683bcba4a108931b76eba4eee828b1d6a8e
thewayiam <twly.tw@gmail.com>
2014-06-28 16:55:14 +0800

    taipei_bills crawler done

--- crawler/crawl_all.sh
+++ crawler/crawl_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-rm -f ../data/*.json     
+rm -f ../data/taipei/*.json     
 scrapy crawl taipei_meeting -o ../data/taipei_meeting_minutes-11.json -t json        
 scrapy crawl taipei_councilor -o ../data/taipei_councilor-11.json -t json  
+scrapy crawl taipei_bills -o ../data/taipei/bills-11.json -t json
--- crawler/taipei/spiders/bills_spider.py
+++ crawler/taipei/spiders/bills_spider.py
@@ -7,11 +7,16 @@ from scrapy.spider import BaseSpider
 from taipei.items import Bills
 
 
-def take_first(list_in):
-    if len(list_in) == 1:
-        return list_in[0]
+def GetDate(text):
+    matchTerm = re.search(u'''
+        (?P<year>[\d]+)[\s]*年[\s]*
+        (?P<month>[\d]+)[\s]*月[\s]*
+        (?P<day>[\d]+)
+    ''', text, re.X)
+    if matchTerm:
+        return '%04d-%02d-%02d' % (int(matchTerm.group('year'))+1911, int(matchTerm.group('month')), int(matchTerm.group('day')))
     else:
-        raise
+        return None
 
 class Spider(BaseSpider):
     #for scrapy
@@ -25,8 +30,8 @@ class Spider(BaseSpider):
             'omastext': u'',
             'rdoDE': u'0',
             'OmasDetr': u'11',
-            'OmasDetp': u'0',
-            'OmasDetm': u'01',
+            'OmasDetp': u'',
+            'OmasDetm': u'',
             'sDateY': u'',
             'sDateM': u'',
             'sDateD': u'',
@@ -38,18 +43,56 @@ class Spider(BaseSpider):
         return [FormRequest("http://tccmis.tcc.gov.tw/OM/OM_SearchList.asp", formdata=payload, callback=self.parse)]
 
     def parse(self, response):
-        items = []
         sel = Selector(response)
+        items = []
         nodes = sel.xpath('//div/table/tr[@id="tr"]')
         for node in nodes:
             item = Bills()
-            td = nodes.xpath('td/text()').extract()
-            item['id'] = td[2].encode('latin1').decode('big5')
-            item['type'] = td[3].encode('latin1').decode('big5')
-            request = Request('%s%s' % ("http://tccmis.tcc.gov.tw/OM/OM_SearchDetail.asp?sys_no=", item['id']), callback=self.parse_profile)
+            td = node.xpath('td/text()').extract()
+            item['id'] = td[1].encode('latin1').decode('big5')
+            item['type'] = ''.join(td[2].encode('latin1').decode('big5').split())
+            item['category'] = td[3].encode('latin1').decode('big5')
+            request = Request("http://tccmis.tcc.gov.tw/OM/OM_SearchDetail.asp?sys_no=%s" % item['id'], callback=self.parse_profile)
             request.meta['item'] = item
             yield request
 
     def parse_profile(self, response):
         sel = Selector(response)
+        item = response.request.meta['item']
+        nodes = sel.xpath('//div[@id="detail"]/table/tr')
+
+        for node in nodes:
+            if node.xpath('td/text()')[0].re(u'目前處理程序'):
+                item['last_action'] = node.xpath('td/div/text()').extract()[0].strip().split(u'、')
+            if node.xpath('td/text()')[0].re(u'提案人'):
+                item['proposed_by'] = node.xpath('td/div/text()').extract()[0].strip().split(u'、')
+            if node.xpath('td/text()')[0].re(u'召集人/委員'):
+                item['proposed_by'] = node.xpath('td/text()').extract()[1].strip().split(u'、')
+            if node.xpath('td/text()')[0].re(u'議決會次'):
+                item['resolusion_date'] = GetDate(node.xpath('td/text()').extract()[1].split()[0])
+                item['resolusion_sitting'] = ''.join(node.xpath('td/text()').extract()[1].split()[1:])
+            if node.xpath('td/text()')[0].re(u'議決文'):
+                item['resolusion'] = node.xpath('td/text()').extract()[1]
+            if node.xpath('td/text()')[0].re(u'案(\s|　)+?號'):
+                item['bill_no'] = node.xpath('td/text()').extract()[1].strip()
+            if node.xpath('td/text()')[0].re(u'來文文號'):
+                td = node.xpath('td/text()').extract()[1].split()
+                item['intray_date'] = GetDate(td[0])
+                if len(td) > 1:
+                    item['intray_no'] = td[1]
+            if node.xpath('td/text()')[0].re(u'收文日期'):
+                item['receipt_date'] = GetDate(node.xpath('td/text()').extract()[1])
+            if node.xpath('td/text()')[0].re(u'審查日期'):
+                item['examination_date'] = node.xpath('td/text()').extract()[1]
+            if node.xpath('td/text()')[0].re(u'審查意見'):
+                item['examination'] = '\n'.join(node.xpath('td/text()').extract()[1:])
+            if node.xpath('td/text()')[0].re(u'發文文號'):
+                td = node.xpath('td/text()').extract()[1].split()
+                item['dispatch_date'] = GetDate(td[0])
+                if len(td) > 1:
+                    item['dispatch_no'] = td[1]
+            if node.xpath('td/text()')[0].re(u'執行情形'):
+                item['execution'] = node.xpath('td/text()').extract()[1]
+            if node.xpath('td/text()')[0].re(u'備[\s]*?註'):
+                item['remark'] = '\n'.join(node.xpath('td/text()').extract()[1:])
         return item
