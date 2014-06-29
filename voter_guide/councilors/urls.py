149d43f76c32cbec84d0063e27cf49a878b6d207
thewayiam <twly.tw@gmail.com>
2014-06-29 15:39:36 +0800

    index_order by bills

--- voter_guide/councilors/urls.py
+++ voter_guide/councilors/urls.py
@@ -4,7 +4,7 @@ from councilors import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, {"index": 'cs_attend', "county": u'臺北市', "ad": 11}),
-    url(r'^(?P<index>cs_attend)/(?P<county>\S+)/(?P<ad>\d+)/$', views.index, name='index'),
+    url(r'^(?P<index>bills|cs_attend)/(?P<county>\S+)/(?P<ad>\d+)/$', views.index, name='index'),
     url(r'^platform/(?P<councilor_id>\S+)/(?P<ad>\d+)/$', views.platformer, name='platformer'),
     #url(r'^report/(?P<index>cs_attend)/(?P<county>\S+)/(?P<ad>\d+)/$', views.chart_report, name='chart_report'),
 )
--- voter_guide/councilors/views.py
+++ voter_guide/councilors/views.py
@@ -6,19 +6,37 @@ from sittings.models import Sittings
 
 
 def index(request, index, county, ad):
-    query = Q(ad=ad, county=county)
+    basic_query = Q(ad=ad, county=county)
     param = {
         'cs_attend': {
-            'title': u'議會開會缺席次數(多→少)'
-        }
+            'title': u'議會開會缺席次數(多→少)',
+            'tooltip': u'缺席',
+            'unit': u'次'
+        },
+        'bills': {
+            'title': u'議案數(多→少)',
+            'tooltip': u'議案',
+            'unit': u'個'
+        },
     }
     if index == 'cs_attend':
-        compare = Sittings.objects.filter(query & Q(committee='')).count()
-        councilors = CouncilorsDetail.objects.filter(query & Q(in_office=True, attendance__category='CS', attendance__status='absent'))\
+        compare = Sittings.objects.filter(basic_query & Q(committee='')).count()
+        councilors = CouncilorsDetail.objects.filter(basic_query & Q(in_office=True, attendance__category='CS', attendance__status='absent'))\
                                              .annotate(totalNum=Count('attendance__id'))\
                                              .order_by('-totalNum','party')
-        no_count_list = CouncilorsDetail.objects.filter(query).exclude(councilor_id__in=councilors.values_list('councilor_id', flat=True))
-    return render(request,'councilors/index/index_ordered.html', {'param': param.get(index), 'ad': ad, 'county': county, 'no_count_list': no_count_list, 'councilors': councilors, 'compare': compare, 'index': index})
+        no_count_list = CouncilorsDetail.objects.filter(basic_query)\
+                                                .exclude(councilor_id__in=councilors.values_list('councilor_id', flat=True))
+        return render(request,'councilors/index/index_ordered.html', {'param': param.get(index), 'ad': ad, 'county': county, 'no_count_list': no_count_list, 'councilors': councilors, 'compare': compare, 'index': index})
+    elif index == 'bills':
+        proposertype = request.GET.get('proposertype', False)
+        query = basic_query & Q(councilors_bills__priproposer=not proposertype)
+        councilors = CouncilorsDetail.objects.filter(query)\
+                                     .annotate(totalNum=Count('councilors_bills__id'))\
+                                     .exclude(totalNum=0)\
+                                     .order_by('-totalNum')
+        no_count_list = CouncilorsDetail.objects.filter(basic_query)\
+                                                .exclude(councilor_id__in=councilors.values_list('councilor_id', flat=True))
+        return render(request,'councilors/index/index_ordered.html', {'param': param.get(index), 'ad': ad, 'county': county, 'proposertype': proposertype, 'no_count_list': no_count_list, 'councilors': councilors, 'index': index})
 
 def platformer(request, councilor_id, ad):
     try:
