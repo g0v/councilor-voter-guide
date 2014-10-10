05ae96770804c76012f3138609a62ae5a10ce5b3
thewayiam <twly.tw@gmail.com>
2014-10-11 00:14:57 +0800

    #13: css static

--- voter_guide/suggestions/views.py
+++ voter_guide/suggestions/views.py
@@ -3,19 +3,19 @@ import operator
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Count, Sum, F, Q
-from .models import Suggestions, Councilors_Suggestions
+from .models import Suggestions
 from councilors.models import CouncilorsDetail
 
 
 def report(request):
-    base = CouncilorsDetail.objects.filter(election_year='2010', county__in=[u'臺北市', u'高雄市'])
-    councilors = base.annotate(sum=Sum('suggestions__suggestion__approved_expense'), count=Count('suggestions__id'))\
-                    .order_by('-sum')
-    parties = base.values('party')\
-                    .annotate(sum=Sum('suggestions__suggestion__approved_expense'), count=Count('suggestions__id'))\
-                    .order_by('-sum')
+    councilors= CouncilorsDetail.objects.filter(election_year='2010', county__in=[u'臺北市', u'高雄市'])
+    councilors = councilors.annotate(sum=Sum('suggestions__suggestion__approved_expense'), count=Count('suggestions__id'))\
+                        .order_by('-sum')
+    parties = councilors.values('party')\
+                        .annotate(sum=Sum('suggestions__suggestion__approved_expense'), count=Count('suggestions__id'))\
+                        .order_by('-sum')
     counties = Suggestions.objects.filter(election_year='2010', county__in=[u'臺北市', u'高雄市'])\
-                    .values('county', 'suggest_year')\
-                    .annotate(sum=Sum('approved_expense'), count=Count('uid'))\
-                    .order_by('county', 'suggest_year')
+                        .values('county', 'suggest_year')\
+                        .annotate(sum=Sum('approved_expense'), count=Count('uid'))\
+                        .order_by('county', 'suggest_year')
     return render(request,'suggestions/report.html', {'councilors': councilors, 'parties': list(parties), 'counties': list(counties)})
