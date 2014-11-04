d8aa3521197f45401b980feeba04d7b627c7d4ae
thewayiam <twly.tw@gmail.com>
2014-11-04 22:29:24 +0800

    ntcc meeting_minutes crawler

--- crawler/README.md
+++ crawler/README.md
@@ -7,21 +7,17 @@
 
 ## 各縣市
 以下缺漏，可能也是暫時找不到資料來源        
-
--	tcc: 臺北市
--	ntcc: 新北市        
-    >缺meeting_minutes      
--	kcc: 高雄市     
-    >缺councilors_terms     
--	tccc: 臺中市        
-    >缺meeting_minutes      
--	hsinchucc: 新竹市       
-    >缺councilors_terms     
-    >缺bills        
-    >缺meeting_minutes      
--	ilcc: 宜蘭縣        
-    >缺councilors_terms     
--	tycc: 桃園縣        
-    >缺councilors_terms     
-    >缺bills        
-    >缺meeting_minutes      
+<table>
+<thead>
+<tr><th>縣市</th><th>代號</th><th>現任議員</th><th> 歷屆議員</th><th>議案</th><th>議事錄</th></tr>
+</thead>
+<tbody>
+<tr><td>臺北市</td><td>tcc</td><td></td><td></td><td></td><td></td></tr>
+<tr><td>新北市</td><td>ntcc</td><td></td><td></td><td></td><td></td></tr>
+<tr><td>高雄市</td><td>kcc</td><td></td><td></td><td>x</td><td></td></tr>
+<tr><td>臺中市</td><td>tccc</td><td></td><td>x</td><td></td><td></td></tr>
+<tr><td>新竹市</td><td>hsinchucc</td><td></td><td>x</td><td>x</td><td>x</td></tr>
+<tr><td>宜蘭縣</td><td>ilcc</td><td></td><td>x</td><td></td><td></td></tr>
+<tr><td>桃園縣</td><td>tycc</td><td></td><td>x</td><td>x</td><td>x</td></tr>
+</tbody>
+</table>
---
+++ crawler/ntcc/ntcc/spiders/meeting_minutes.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import re
+import urllib
+import codecs
+import subprocess
+from urlparse import urljoin
+import scrapy
+from scrapy.http import Request, FormRequest
+from scrapy.selector import Selector
+from ntcc.items import MeetingMinutes
+
+
+def ROC2AD(text):
+    matchTerm = re.search(u'''
+        (?P<year>[\d]+)[\s]*(?:年|[-/.])[\s]*
+        (?P<month>[\d]+)[\s]*(?:月|[-/.])[\s]*
+        (?P<day>[\d]+)
+    ''', text, re.X)
+    if matchTerm:
+        return '%04d-%02d-%02d' % (int(matchTerm.group('year'))+1911, int(matchTerm.group('month')), int(matchTerm.group('day')))
+    else:
+        return None
+
+def write_file(data, file_name):
+    file = codecs.open(file_name, 'w', encoding='utf-8')
+    file.write(data)
+    file.close()
+
+class Spider(scrapy.Spider):
+    name = "meeting"
+    allowed_domains = ["www.ntp.gov.tw"]
+    start_urls = ['http://www.ntp.gov.tw/content/information/information04.aspx']
+    download_delay = 1
+
+    def parse(self, response):
+        for node in response.xpath(u'//a[contains(@title, "HTML檔")]/@href').extract():
+            yield Request(node, callback=self.parse_sitting)
+
+    def parse_sitting(self, response):
+        for node in response.xpath(u'//a/@href').extract():
+            yield Request(urljoin(response.url, node), callback=self.parse_meeting)
+
+    def parse_meeting(self, response):
+        sitting = response.xpath('//head/title/text()').re(u'(.+)日程表')[0]
+        trs = [tr for tr in response.xpath('//table/tr') if tr.xpath('td[3]/text()').re('\d+')]
+        for tr in trs:
+            item = MeetingMinutes()
+            item['county'] = u'新北市'
+            item['date'] = ROC2AD(tr.xpath('td[1]/text()').extract()[0])
+            item['sitting'] = sitting
+            item['meeting'] = tr.xpath('td[3]/text()').extract()[0]
+            item['download_url'] = urljoin(response.url, tr.xpath('td[6]/a[1]/@href').extract()[0])
+            if re.search('\.pdf$', item['download_url']):
+                continue
+                yield Request(item['download_url'], callback=self.download_pdf, meta={'item': item})
+            elif re.search('\.htm$', item['download_url']):
+                yield Request(item['download_url'], callback=self.parse_html, meta={'item': item})
+
+    def download_pdf(self, response):
+        item = response.meta['item']
+        cmd = 'wget -c -O ../../meeting_minutes/ntcc/%s_%s.pdf %s' % (item['sitting'], item['meeting'], item['download_url'])
+        retcode = subprocess.call(cmd, shell=True)
+        return item
+
+    def parse_html(self, response):
+        item = response.meta['item']
+        text = '\n'.join(response.xpath('//pre/text()').extract())
+        write_file(text, '../../meeting_minutes/ntcc/%s_%s.txt' % (item['sitting'], item['meeting']))
+        return item
