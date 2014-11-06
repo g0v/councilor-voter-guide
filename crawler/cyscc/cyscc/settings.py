97c321ed64d71562e2366c3af9dedce0546d3e87
starsdog <ling@yilingdeMacBook-Pro.local>
2014-11-07 00:02:54 +0800

    cyscc basic parser

---
+++ crawler/cyscc/cyscc/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for cyscc project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'cyscc'
+
+SPIDER_MODULES = ['cyscc.spiders']
+NEWSPIDER_MODULE = 'cyscc.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'cyscc (+http://www.yourdomain.com)'
