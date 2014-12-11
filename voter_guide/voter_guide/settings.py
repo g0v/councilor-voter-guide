d437c622ea8eebd9f6bd147ae0f7d28d85c82a73
thewayiam <twly.tw@gmail.com>
2014-12-11 11:58:53 +0800

    #71: political_contribution d3 pie chart text overlap fix by @ChiaYinT

--- voter_guide/voter_guide/settings.py
+++ voter_guide/voter_guide/settings.py
@@ -101,9 +101,6 @@ if not DEBUG:
         }
     }
 
-# for django-debug-toolbar
-INTERNAL_IPS = ('127.0.0.1',)
-
 MIDDLEWARE_CLASSES = (
     #'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -150,8 +147,6 @@ INSTALLED_APPS = (
     'commontag',
     'pagination',
     'rest_framework',
-    'south',
-    'debug_toolbar',
 )
 
 REST_FRAMEWORK = {
