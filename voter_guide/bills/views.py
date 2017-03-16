761f702f0196b9c9ed6cba50d23b3e2267bdb170
thewayiam <twly.tw@gmail.com>
2017-03-16 18:05:02 +0800

    fixed order of county list

--- voter_guide/bills/views.py
+++ voter_guide/bills/views.py
@@ -17,8 +17,14 @@ def split_unicode_chrs( text ):
   return [ chr for chr in _unicode_chr_splitter( text ) if (chr!=' ' and chr) ]
 
 def select_county(request, index, county):
-    counties = CouncilorsDetail.objects.values_list('county', flat=True).order_by('county').distinct()
-    return render(request, 'bills/select_county.html', {'index': index, 'counties': counties})
+    regions = [
+        {"region": "北部", "counties": ["臺北市", "新北市", "桃園市", "基隆市", "宜蘭縣", "新竹縣", "新竹市"]},
+        {"region": "中部", "counties": ["苗栗縣", "臺中市", "彰化縣", "雲林縣", "南投縣"]},
+        {"region": "南部", "counties": ["嘉義縣", "嘉義市", "臺南市", "高雄市", "屏東縣"]},
+        {"region": "東部", "counties": ["花蓮縣", "臺東縣"]},
+        {"region": "離島", "counties": ["澎湖縣", "金門縣", "連江縣"]}
+    ]
+    return render(request, 'bills/select_county.html', {'index': index, 'regions': regions})
 
 
 def bills(request, county, index):
--- voter_guide/candidates/views.py
+++ voter_guide/candidates/views.py
@@ -5,10 +5,14 @@ from .models import Candidates
 
 
 def counties(request, election_year):
-    counties = Candidates.objects.filter(election_year=election_year).values('county')\
-                                 .annotate(constituency=Count('constituency', distinct = True), candidates=Count('id'))\
-                                 .order_by('-candidates')
-    return render(request, 'candidates/counties.html', {'election_year': election_year, 'counties': counties})
+    regions = [
+        {"region": "北部", "counties": ["臺北市", "新北市", "桃園市", "基隆市", "宜蘭縣", "新竹縣", "新竹市"]},
+        {"region": "中部", "counties": ["苗栗縣", "臺中市", "彰化縣", "雲林縣", "南投縣"]},
+        {"region": "南部", "counties": ["嘉義縣", "嘉義市", "臺南市", "高雄市", "屏東縣"]},
+        {"region": "東部", "counties": ["花蓮縣", "臺東縣"]},
+        {"region": "離島", "counties": ["澎湖縣", "金門縣", "連江縣"]}
+    ]
+    return render(request, 'candidates/counties.html', {'election_year': election_year, 'regions': regions})
 
 def districts(request, election_year, county):
     districts = Candidates.objects.filter(election_year=election_year, county=county).values('constituency', 'district')\
