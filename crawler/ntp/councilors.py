008b7350f2b213c241be8c1286ad96de86af458e
thewayiam <twly.tw@gmail.com>
2017-03-20 21:20:54 +0800

    ntp councilor crawler title bug fix

--- crawler/ntp/councilors.py
+++ crawler/ntp/councilors.py
@@ -12,19 +12,15 @@ class Spider(scrapy.Spider):
     start_urls = ["http://www.ntp.gov.tw/content/list/list02.aspx"]
 
     def __init__(self):
-        self.url_root = "http://www.ntp.gov.tw/content/list"
-        with open(os.path.join(os.path.dirname(__file__), 'constituency.json'), 'r') as fh:
-            self.constituency = json.loads(fh.read())
+        with open(os.path.join(os.path.dirname(__file__), 'constituency.json'), 'r') as infile:
+            self.constituency = json.loads(infile.read())
 
     def parse(self, response):
         for url in response.xpath('//*[@id="main"]/descendant::tr/td[2]/a/@href').extract():
-            yield scrapy.Request('%s/%s' % (self.url_root, url),
-                callback=self.parse_profile
-            )
+            yield scrapy.Request(urljoin(response.url, url), callback=self.parse_profile)
 
     def parse_profile(self, response):
         c = {}
-
         c['image'] = urljoin(
             response.url,
             urllib.quote(response.xpath('//*[@id="main"]/descendant::img[contains(@src, "CouncilorFile")]').extract_first().encode('utf8'))
@@ -32,18 +28,18 @@ class Spider(scrapy.Spider):
         c['links'] = [{'url': response.url, 'note': u'議會個人官網'}]
         c['constituency'] = response.xpath(u'//*[@id="printContext"]/descendant::a[re:test(text(), "第\d+選區")]/text()').extract_first()
         c['county'] = u'新北市'
+        c['election_year'] = '2014'
+        c['in_office'] = True
+        c['term_start'] = '%s-12-25' % c['election_year']
+        c['term_end'] = {'date': '2018-12-24'}
+        c['links'] = {'note': u'個人網站', 'url': response.url}
         c['district'] = self.constituency[c['constituency']]
         c['name'] = response.xpath(u'//td[contains(text(), "姓名")]/following-sibling::td[1]/text()').extract_first()
         c['party'] = response.xpath(u'//td[contains(text(), "政黨")]/following-sibling::td[1]/text()').extract_first()
-        c['links'] = {
-            'note': u'個人網站',
-            'url': response.url
-        }
-        c['education'] = [x.strip() for x in response.xpath(u'//img[@alt="學歷"]/ancestor::tr[1]/following-sibling::tr[1]/descendant::td/text()').extract()]
-        c['experience'] = [x.strip() for x in response.xpath(u'//img[@alt="經歷"]/ancestor::tr[1]/following-sibling::tr[1]/descendant::td/text()').extract()]
-        c['title'] = response.xpath(u'//img[@alt="現任"]/ancestor::tr[1]/following-sibling::tr[1]/descendant::td/text()').re(u'第\d+屆(.+)')[0]
-        c['platform'] = [x.strip() for x in response.xpath(u'//img[@alt="政見"]/ancestor::tr[1]/following-sibling::tr[1]/descendant::td/text()').extract()]
-
+        c['education'] = [x.strip() for x in response.xpath(u'//img[@alt="學歷"]/ancestor::tr[1]/following-sibling::tr[1]/descendant::td/text()').extract() if x.strip()]
+        c['experience'] = [x.strip() for x in response.xpath(u'//img[@alt="經歷"]/ancestor::tr[1]/following-sibling::tr[1]/descendant::td/text()').extract() if x.strip()]
+        c['title'] = response.xpath(u'//img[@alt="現任"]/ancestor::tr[1]/following-sibling::tr[1]/descendant::td/text()').re(u'(副?議長|議員)')[0]
+        c['platform'] = [x.strip() for x in response.xpath(u'//img[@alt="政見"]/ancestor::tr[1]/following-sibling::tr[1]/descendant::td/text()').extract() if x.strip()]
         c['contact_details'] = []
         contact_mappings = {
             u'電話': 'voice',
@@ -57,11 +53,4 @@ class Spider(scrapy.Spider):
                 'type': name,
                 'value': response.xpath(u'string(//td[contains(text(), "%s")]/following-sibling::td[1])' % label).extract_first()
             })
-
-        ## some dirty hand items
-        c['election_year'] = '2014'
-        c['in_office'] = True
-        c['term_start'] = '%s-12-25' % c['election_year']
-        c['term_end'] = {'date': '2018-12-24'}
-
         yield c
