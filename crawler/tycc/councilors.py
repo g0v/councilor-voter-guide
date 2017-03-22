c7175a7ec4d07842101dcb370e2d5105421daee6
thewayiam <twly.tw@gmail.com>
2017-03-22 22:49:20 +0800

    tycc councilor missing constituency in some region bug fix

--- crawler/tycc/councilors.py
+++ crawler/tycc/councilors.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+import json
 import re
 import urllib
 from urlparse import urljoin
@@ -11,18 +13,25 @@ class Spider(scrapy.Spider):
     start_urls = ["http://www.tycc.gov.tw/page.aspx?wtp=1&wnd=204", ]
     download_delay = 0.5
 
+    def __init__(self):
+        with open(os.path.join(os.path.dirname(__file__), '../../data/cand-moi-direct-control-2018.json'), 'r') as infile:
+            self.ref = {re.sub(u'[\s　]', '', person['idname']): person for person in json.loads(infile.read()) if person['cityname'] == u'桃園市'}
+
     def parse(self, response):
         urls = response.xpath('//map/area[@alt]/@href').extract()
         for url in urls:
             yield scrapy.Request(urljoin(response.url, url), callback=self.parse_selection_index)
 
     def parse_selection_index(self, response):
+        selected_count = len(response.xpath('//*[@id="ctl00_ContentPlaceHolder1_Members1_DDL_AREA"]/option[@selected]/preceding-sibling::option')) + 1
+        constituency = u'第%d選區' % selected_count
         urls = response.xpath('//img/parent::a[@alt]/@href').extract()
         for url in urls:
-            yield scrapy.Request(urljoin(response.url, url), callback=self.parse_profile)
+            yield scrapy.Request(urljoin(response.url, url), callback=self.parse_profile, meta={'constituency': constituency})
 
     def parse_profile(self, response):
         item = {}
+        item['constituency'] = response.meta['constituency']
         item['county'] = u'桃園市'
         item['election_year'] = '2014'
         item['term_start'] = '%s-12-25' % item['election_year']
@@ -30,14 +39,15 @@ class Spider(scrapy.Spider):
         item['in_office'] = True
         item['name'], item['title'] = response.xpath('//*[@id="ctl00_ContentPlaceHolder1_Members1_LB_MEM_NAME"]/text()').extract_first().split()
         item['links'] = [{'url': response.url, 'note': u'議會個人官網'}]
+        item['party'] = self.ref[item['name']]['partymship']
+        item['gender'] = self.ref[item['name']]['sex']
         img_url = response.xpath('//img[@class="memImg"]/@src').extract_first()
         item['image'] = urljoin(response.url, urllib.quote(img_url.encode('utf8')))
         region = response.xpath('//*[@id="ctl00_ContentPlaceHolder1_Members1_LB_MEM_AREA"]/text()').extract_first().split()
         # e.g. "平地原住民" vs "第一選區 桃園區"
         item['district'] = region[1] if len(region) > 1 else region[0]
-        item['constituency'] = region[0] if len(region) > 1 else ''
         item['education'] = [x.strip() for x in response.xpath('//*[@id="ctl00_ContentPlaceHolder1_Members1_LB_MEM_SCHOOL"]/text()').extract() if x.strip()]
-        item['education'] = [x.strip() for x in response.xpath('//*[@id="ctl00_ContentPlaceHolder1_Members1_LB_MEM_WORK"]/text()').extract() if x.strip()]
+        item['experience'] = [x.strip() for x in response.xpath('//*[@id="ctl00_ContentPlaceHolder1_Members1_LB_MEM_WORK"]/text()').extract() if x.strip()]
 
         fb_links = response.xpath('//*[@id="ctl00_ContentPlaceHolder1_Members1_LB_MEM_FACEBOOK"]/descendant::a/@href').extract()
         for fb_link in fb_links:
