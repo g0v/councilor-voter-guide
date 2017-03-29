06ca2cc7435ed0e9f39fe3a58f6328597100f308
thewayiam <twly.tw@gmail.com>
2017-03-29 19:12:07 +0800

    tcc suggestions crawler

--- crawler/tcc/README.md
+++ crawler/tcc/README.md
@@ -1,4 +1,5 @@
 ```
+scrapy runspider suggestions.py -o ../../data/tcc/suggestions.json -s FEED_EXPORT_ENCODING=utf-8
 scrapy runspider councilors.py -o ../../data/tcc/councilors.json -s FEED_EXPORT_ENCODING=utf-8
 scrapy runspider councilors_terms.py -o ../../data/tcc/councilors_terms.json -s FEED_EXPORT_ENCODING=utf-8
 scrapy runspider bills.py -o ../../data/tcc/bills.json -s FEED_EXPORT_ENCODING=utf-8
---
+++ crawler/tcc/suggestions.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import urllib
+from urlparse import urljoin
+import subprocess
+
+import scrapy
+
+
+class Spider(scrapy.Spider):
+    name = 'suggestions'
+    start_urls = ["http://dbas.gov.taipei/"]
+
+    def parse(self, response):
+        yield scrapy.Request(urljoin(response.url, response.xpath(u'//a[@title="政府資訊公開"]/@href').extract_first()), callback=self.parse_opengov)
+
+    def parse_opengov(self, response):
+        yield scrapy.Request(urljoin(response.url, response.xpath(u'//a[contains(@title, "議員所提地方建設建議事項")]/@href').extract_first()), callback=self.parse_file)
+
+    def parse_file(self, response):
+        for node in response.xpath(u'//a[contains(@title, "議員所提")]'):
+            item = {}
+            m = re.search(u'(?P<year>\d+)年度.*?(?P<month_to>\d+)月', node.xpath('@title').extract_first())
+            item['year'] = int(m.group('year')) + 1911
+            item['month_to'] = m.group('month_to').zfill(2)
+            item['month_from'] = '%02d' % (int(m.group('month_to')) - 5)
+            item['url'] = urljoin(response.url, node.xpath('@href').extract_first())
+            item['file_ext'] = item['url'].split('.')[-1]
+            cmd = u'mkdir -p ../../data/tcc/suggestions/ && wget --heade="User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -Nc -O ../../data/tcc/suggestions/{year}_{month_from}-{month_to}.{file_ext} "{url}"'.format(**item)
+            subprocess.call(cmd, shell=True)
+            yield item
