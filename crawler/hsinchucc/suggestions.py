71fff79727a1a61cf641e7a01f248675c5aa9619
thewayiam <twly.tw@gmail.com>
2017-04-11 17:06:25 +0800

    #148: almost fix

--- crawler/hsinchucc/suggestions.py
+++ crawler/hsinchucc/suggestions.py
@@ -16,19 +16,19 @@ class Spider(scrapy.Spider):
         yield scrapy.Request(urljoin(response.url, response.xpath(u'//a[@title="議員建議事項"]/@href').extract_first()), callback=self.parse_list)
 
     def parse_list(self, response):
-        for url in response.xpath(u'//a[re:test(text(), "\d+月底議員建議事項")]/@href').extract():
+        for url in response.xpath(u'//a[re:test(text(), "12月底議員建議事項")]/@href').extract():
             yield scrapy.Request(urljoin(response.url, url), callback=self.parse_file)
 
     def parse_file(self, response):
         item = {}
         title = response.css(u'.content_detail_title').extract_first()
-        m = re.search(u'(?P<year>\d+)年.*?(?P<month_to>\d+)月', title)
+        m = re.search(u'(?P<year>\d+)年', title)
         item['year'] = int(m.group('year')) + 1911
-        item['month_to'] = m.group('month_to').zfill(2)
-        item['month_from'] = '%02d' % (int(m.group('month_to')) - 5)
+        item['month_to'] = '12'
+        item['month_from'] = '01'
         for node in response.css('.div_file').xpath('a'):
             item['url'] = urljoin(response.url, node.xpath('@href').extract_first())
             item['file_ext'] = re.sub('[()\s]', '', node.xpath('text()').extract_first()).lower()
-            cmd = u'mkdir -p ../../data/hsinchucc/suggestions/ && wget --heade="User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -Nc -O ../../data/hsinchucc/suggestions/{year}_{month_from}-{month_to}.{file_ext} "{url}"'.format(**item)
+            cmd = u'mkdir -p ../../data/hsinchucc/suggestions/ && wget --heade="User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -q -O ../../data/hsinchucc/suggestions/{year}_{month_from}-{month_to}.{file_ext} "{url}"'.format(**item)
             subprocess.call(cmd, shell=True)
             yield item
