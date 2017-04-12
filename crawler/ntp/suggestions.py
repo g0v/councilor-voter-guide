86cb151c72ee720e674df431769602e5c0e0b423
thewayiam <twly.tw@gmail.com>
2017-04-12 19:50:32 +0800

    #146: data fixed from source

--- crawler/ntp/suggestions.py
+++ crawler/ntp/suggestions.py
@@ -13,13 +13,10 @@ class Spider(scrapy.Spider):
     start_urls = ["http://www.bas.ntpc.gov.tw/"]
 
     def parse(self, response):
-        yield scrapy.Request(urljoin(response.url, response.xpath(u'//a[@title="政府資訊公開"]/@href').extract_first()), callback=self.parse_opengov)
-
-    def parse_opengov(self, response):
-        yield scrapy.Request(response.xpath(u'//a[@title="議員所提地方建設建議事項"]/@href').extract_first(), callback=self.parse_file)
+        yield scrapy.Request(urljoin(response.url, response.xpath(u'//a[contains(@title, "議員所提")]/@href').extract_first()), callback=self.parse_file)
 
     def parse_file(self, response):
-        for node in response.xpath(u'//a[contains(@title, "議員所提")]'):
+        for node in response.css(u'a.abc4'):
             item = {}
             m = re.search(u'(?P<year>\d+)年度(?P<month_from>\d+)\s*[-~]+\s*(?P<month_to>\d+)月', node.xpath('normalize-space(string())').extract_first())
             item['year'] = int(m.group('year')) + 1911
@@ -27,6 +24,6 @@ class Spider(scrapy.Spider):
             item['month_to'] = m.group('month_to').zfill(2)
             item['url'] = urljoin(response.url, node.xpath('@href').extract_first())
             item['file_ext'] = item['url'].split('.')[-1]
-            cmd = u'mkdir -p ../../data/ntp/suggestions/ && wget --heade="User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -Nc -O ../../data/ntp/suggestions/{year}_{month_from}-{month_to}.{file_ext} "{url}"'.format(**item)
+            cmd = u'mkdir -p ../../data/ntp/suggestions/ && wget --heade="User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -q -O ../../data/ntp/suggestions/{year}_{month_from}-{month_to}.{file_ext} "{url}"'.format(**item)
             subprocess.call(cmd, shell=True)
             yield item
