5d107abcde9ed6d7d165c0e5968c2715486011bd
thewayiam <twly.tw@gmail.com>
2017-04-13 15:22:15 +0800

    #154: data fixed from source

--- crawler/tccc/suggestions.py
+++ crawler/tccc/suggestions.py
@@ -32,7 +32,7 @@ class Spider(scrapy.Spider):
             if re.search(u'檔案下載', node.xpath('@title').extract_first()):
                 item['url'] = urljoin(response.url, node.xpath('@href').extract_first())
                 item['file_ext'] = item['url'].split('.')[-1]
-                cmd = u'mkdir -p ../../data/tccc/suggestions/ && wget --heade="User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -Nc -O ../../data/tccc/suggestions/{year}_{month_from}-{month_to}.{file_ext} "{url}"'.format(**item)
+                cmd = u'mkdir -p ../../data/tccc/suggestions/ && wget --heade="User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -q -O ../../data/tccc/suggestions/{year}_{month_from}-{month_to}.{file_ext} "{url}"'.format(**item)
                 subprocess.call(cmd, shell=True)
                 yield item
             else:
@@ -51,6 +51,6 @@ class Spider(scrapy.Spider):
                 item['month_from'] = '07'
             item['url'] = urljoin(response.url, node.xpath('@href').extract_first())
             item['file_ext'] = item['url'].split('.')[-1]
-            cmd = u'mkdir -p ../../data/tccc/suggestions/ && wget --heade="User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -Nc -O ../../data/tccc/suggestions/{year}_{month_from}-{month_to}.{file_ext} "{url}"'.format(**item)
+            cmd = u'mkdir -p ../../data/tccc/suggestions/ && wget --heade="User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -q -O ../../data/tccc/suggestions/{year}_{month_from}-{month_to}.{file_ext} "{url}"'.format(**item)
             subprocess.call(cmd, shell=True)
             yield item
