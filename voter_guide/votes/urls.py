121aef989ce860473525fc71b8d0f228be048099
thewayiam <twly.tw@gmail.com>
2017-06-01 16:29:44 +0800

    vote url routing bug fix

--- voter_guide/votes/urls.py
+++ voter_guide/votes/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from votes import views
 
 urlpatterns = [
+    url(r'^detail/(?P<vote_id>\S+)/$', views.vote, name='vote'),
     url(r'^(?P<county>\S+)/(?P<index>normal|conscience)/$', views.votes, name='votes'),
     url(r'^(?P<county>\S+)/$', views.votes, {'index': 'normal'}, name='votes'),
-    url(r'^(?P<vote_id>\S+)/$', views.vote, name='vote'),
 ]
