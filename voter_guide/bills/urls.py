8eb2de30da240f18c44e7625c573b41a952a0d62
thewayiam <twly.tw@gmail.com>
2017-06-01 16:32:47 +0800

    bill url routing bug fix

--- voter_guide/bills/urls.py
+++ voter_guide/bills/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from bills import views
 
 urlpatterns = [
+    url(r'^detail/(?P<county>\S+)/(?P<bill_id>\S+)/$', views.bill_detail, name='bill_detail'),
     url(r'^(?P<county>\S+)/(?P<index>normal)/$', views.bills, name='bills'),
     url(r'^(?P<county>\S+)/$', views.bills, {'index': 'normal'}, name='bills'),
     url(r'^(?P<county>\S+)/(?P<index>normal)/(?P<category>.+)$', views.bills_category, name='bills_category'),
-    url(r'^(?P<county>\S+)/(?P<bill_id>\S+)/$', views.bill_detail, name='bill_detail'),
 ]
