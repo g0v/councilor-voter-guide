c8cd5fb77a3bac750797581df19a2c6ca4e10d1d
thewayiam <twly.tw@gmail.com>
2017-07-18 16:27:32 +0800

    bills could search by tag

--- voter_guide/bills/views.py
+++ voter_guide/bills/views.py
@@ -31,8 +31,9 @@ def select_county(request, index, county):
 
 def bills(request, county):
     query = Q(county=county)
+    query = query & Q(uid__in=Standpoints.objects.filter(title=request.GET['tag']).exclude(bill__isnull=True).values_list('bill_id', flat=True).distinct()) if request.GET.get('tag') else query
     keyword = request.GET.get('keyword', '')
-    district = request.GET.get('district', None)
+    district = request.GET.get('district')
     if keyword:
         bills = Bills.objects.filter(query & reduce(operator.and_, (Q(abstract__icontains=x) for x in split_unicode_chrs(keyword))))
         if bills:
@@ -49,7 +50,8 @@ def bills(request, county):
                  .order_by('-election_year', '-uid')
     bills = paginate(request, bills)
     districts = CouncilorsDetail.objects.filter(county=county).filter(~Q(district='')).order_by('constituency').values_list('district', flat=True).distinct()
-    return render(request, 'bills/bills.html', {'county': county, 'keyword_hot': keyword_list('bills'), 'category': None, 'bills': bills, 'districts': districts})
+    standpoints = Standpoints.objects.filter(county=county).exclude(bill__isnull=True).values_list('title', flat=True).order_by('-pro').distinct()
+    return render(request, 'bills/bills.html', {'county': county, 'keyword_hot': keyword_list('bills'), 'category': None, 'bills': bills, 'districts': districts, 'hot_standpoints': standpoints[:5]})
 
 def bill_detail(request, county, bill_id):
     bill = get_object_or_404(Bills, county=county, uid=bill_id)
