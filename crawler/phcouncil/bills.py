9affa9542e02a253b1761d137c577b1f4381a0ea
thewayiam <twly.tw@gmail.com>
2018-01-29 15:28:45 +0800

    phcouncil bills crawler proposed_by bug fix

--- crawler/phcouncil/bills.py
+++ crawler/phcouncil/bills.py
@@ -15,8 +15,7 @@ class Spider(scrapy.Spider):
     start_urls = ["http://www.phcouncil.gov.tw/"]
     download_delay = 0.5
     county_abbr = os.path.dirname(os.path.realpath(__file__)).split('/')[-1]
-    election_year = '2009'
-#   election_year = common.election_year(county_abbr)
+    election_year = common.election_year(county_abbr)
     ads = {'2009': 17, '2014': 18, '2018': 19}
     ad = ads[election_year]
 
@@ -42,16 +41,16 @@ class Spider(scrapy.Spider):
         item = {}
         item['election_year'] = self.election_year
         item['type'] = response.meta['type']
-        item['id'] = re.search(u'id=([^&]*)', response.url).group(1)
-        for key, label in [('category', u'類[\s　]*別'), ('abstract', u'案[\s　]*由'), ('description', u'說[\s　]*明'), ('methods', u'辦[\s　]*法'), ('execution', u'決[\s　]*議')]:
-                content = response.xpath(u'string((//*[re:test(., "%s")]/following-sibling::td)[1])' % label).extract_first()
-                if content:
-                    item[key] = content.strip()
+        item['id'] = '%s-%s' % (self.election_year, re.search(u'id=([^&]*)', response.url).group(1))
+        for key, label in [('category', u'類[\s　]*別'), ('abstract', u'案[\s　]*由'), ('description', u'說[\s　]*明'), ('methods', u'辦[\s　]*法'), ('execution', u'決[\s　]*議'), ('execution', u'議[\s　]*決')]:
+            content = response.xpath(u'string((//*[re:test(., "%s")]/following-sibling::td)[1])' % label).extract_first()
+            if content:
+                item[key] = content.strip()
         if item['type'] == u'縣府提案':
             item['proposed_by'] = u'縣府'
         else:
-            item['proposed_by'] = re.split(u'[，、 ]', re.sub(u'(副?議長|議員)', '', response.xpath(u'(//*[re:test(., "(提[\s　]*案|動[\s　]*議|請[\s　]*願)[\s　]*人")]/following-sibling::td)[1]/text()').extract_first()).strip())
-        item['petitioned_by'] = re.split(u'[，、 ]', re.sub(u'(副?議長|議員)', '', (response.xpath(u'(//*[re:test(., "(連[\s　]*署|附[\s　]*議)[\s　]*人")]/following-sibling::td)[1]/text()').extract_first() or '')).strip())
+            item['proposed_by'] = re.split(u'[，、 ]', re.sub(u'(副?議長|議員)', '', response.xpath(u'//*[re:test(., "(提[\s　]*案|動[\s　]*議|請[\s　]*願)[\s　]*人")]')[-1].xpath('following-sibling::td[1]/text()').extract_first()).strip())
+        item['petitioned_by'] = re.split(u'[，、 ]', re.sub(u'(副?議長|議員)', '', response.xpath(u'//td[re:test(., "(連[\s　]*署|附[\s　]*議)[\s　]*人")]')[-1].xpath('following-sibling::td[1]/text()').extract_first()).strip()) if len(response.xpath(u'//*[re:test(., "(連[\s　]*署|附[\s　]*議)[\s　]*人")]')) > 0 else ['']
         item['links'] = [
             {
                 'url': response.url,
