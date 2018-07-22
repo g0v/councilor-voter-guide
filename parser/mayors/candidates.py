770c80e4b3b0d1a02daa6e258f053280fdc2d811
thewayiam <twly.tw@gmail.com>
2018-07-23 00:55:46 +0800

    2014 mayors candidates data update

--- parser/mayors/candidates.py
+++ parser/mayors/candidates.py
@@ -3,12 +3,10 @@
 import sys
 sys.path.append('../')
 import re
+import os
 import json
-import glob
-import psycopg2
-from datetime import datetime
-import ast
-from sys import argv
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 import db_settings
 import common
@@ -28,32 +26,109 @@ def upsertCandidates(candidate):
         SET name = %(name)s, birth = %(birth)s, identifiers = %(identifiers)s
     ''', complement)
     c.execute('''
-        INSERT INTO candidates_terms(uid, candidate_id, type, election_year, number, name, gender, party, constituency, county, district, contact_details, education, experience, remark, image, links, platform, votes, votes_percentage, votes_detail, elected, occupy)
-        VALUES (%(candidate_term_uid)s, %(candidate_uid)s, %(type)s, %(election_year)s, %(number)s, %(name)s, %(gender)s, %(party)s, %(constituency)s, %(county)s, %(district)s, %(contact_details)s, %(education)s, %(experience)s, %(remark)s, %(image)s, %(links)s, %(platform)s, %(votes)s, %(votes_percentage)s, %(votes_detail)s, %(elected)s, %(occupy)s)
+        INSERT INTO candidates_terms(uid, candidate_id, elected_councilor_id, councilor_terms, election_year, number, name, gender, party, constituency, county, district, contact_details, education, experience, remark, image, links, platform, type)
+        VALUES (%(candidate_term_uid)s, %(candidate_uid)s, %(councilor_term_id)s, %(councilor_terms)s, %(election_year)s, %(number)s, %(name)s, %(gender)s, %(party)s, %(constituency)s, %(county)s, %(district)s, %(contact_details)s, %(education)s, %(experience)s, %(remark)s, %(image)s, %(links)s, %(platform)s, %(type)s)
         ON CONFLICT (election_year, candidate_id)
         DO UPDATE
-        SET type = %(type)s, number = %(number)s, name = %(name)s, gender = %(gender)s, party = %(party)s, constituency = %(constituency)s, county = %(county)s, district = %(district)s, contact_details = %(contact_details)s, education = %(education)s, experience = %(experience)s, remark = %(remark)s, image = %(image)s, links = %(links)s, votes = %(votes)s, votes_percentage = %(votes_percentage)s, votes_detail = %(votes_detail)s, elected = %(elected)s, occupy = %(occupy)s
+        SET elected_councilor_id = %(councilor_term_id)s, councilor_terms = %(councilor_terms)s, number = %(number)s, name = %(name)s, gender = %(gender)s, party = %(party)s, constituency = %(constituency)s, county = %(county)s, district = %(district)s, contact_details = %(contact_details)s, education = %(education)s, experience = %(experience)s, remark = %(remark)s, image = %(image)s, links = %(links)s
     ''', complement)
+    terms = []
+    for t in ['mayor', 'legislator', 'councilor']:
+        if candidate.get('%s_terms' % t):
+            for term in candidate['%s_terms' % t]:
+                term['type'] = t
+                terms.append(term)
+    c.execute('''
+        UPDATE candidates_terms
+        SET data = (COALESCE(data, '{}'::jsonb) || %s::jsonb)
+        WHERE election_year = %s and candidate_id = %s
+    ''', [json.dumps({'terms': terms}), complement['election_year'], complement['candidate_uid'], ])
+    if candidate.get('mayor_terms'):
+        c.execute('''
+            UPDATE candidates_terms
+            SET data = (COALESCE(data, '{}'::jsonb) || %s::jsonb)
+            WHERE election_year = %s and candidate_id = %s
+        ''', [json.dumps({'mayor_terms': complement['mayor_terms']}), complement['election_year'], complement['candidate_uid'], ])
+    if candidate.get('legislator_terms'):
+        c.execute('''
+            UPDATE candidates_terms
+            SET data = (COALESCE(data, '{}'::jsonb) || %s::jsonb)
+            WHERE election_year = %s and candidate_id = %s
+        ''', [json.dumps({'legislator_terms': complement['legislator_terms']}), complement['election_year'], complement['candidate_uid'], ])
+    if candidate.get('legislator_data'):
+        c.execute('''
+            UPDATE candidates_terms
+            SET data = (COALESCE(data, '{}'::jsonb) || %s::jsonb)
+            WHERE election_year = %s and candidate_id = %s
+        ''', [json.dumps({'legislator_data': complement['legislator_data']}), complement['election_year'], complement['candidate_uid'], ])
+    if candidate.get('legislator_candidate_info'):
+        c.execute('''
+            UPDATE candidates_terms
+            SET politicalcontributions = COALESCE(politicalcontributions, '[]'::jsonb) || %s::jsonb
+            WHERE election_year >= %s and candidate_id = %s
+        ''', [candidate['legislator_candidate_info']['politicalcontributions'], complement['election_year'], complement['candidate_uid'], ])
+        c.execute('''
+            UPDATE candidates_terms
+            SET politicalcontributions = (SELECT jsonb_agg(x) FROM (
+                SELECT x from (
+                    SELECT DISTINCT(value) as x
+                    FROM jsonb_array_elements(politicalcontributions)
+                ) t ORDER BY x->'election_year' DESC
+            ) tt)
+            WHERE candidate_id = %s AND election_year >= %s
+        ''', [complement['candidate_uid'], complement['election_year'], ])
 
 conn = db_settings.con()
+conn_another = db_settings.con_another()
 c = conn.cursor()
-election_year = ast.literal_eval(argv[1])['election_year']
-county_versions = json.load(open('../county_versions.json'))
-candidates = json.load(open('../../data/candidates/%s/mayors.json' % election_year))
-for candidate in candidates:
-    candidate['type'] = 'mayors'
-    candidate['elected'] = True if re.search(u'[*!]', candidate['elected']) else False
-    candidate['occupy'] = True if re.search(u'是', candidate['occupy']) else False
-    candidate['district'] = candidate['county']
-    candidate['constituency'] = 0
-    candidate['birth'] = candidate['birth_year']
-    candidate['birth'] = datetime.strptime(str(candidate['birth']), '%Y')
-    for county_change in county_versions.get(election_year, []):
-        candidate['previous_county'] = county_change['from'] if candidate['county'] == county_change['to'] else candidate['county']
-    candidate['name'] = common.normalize_person_name(candidate['name'])
-    candidate['party'] = common.normalize_party(candidate['party'])
-    candidate['election_year'] = election_year
-    candidate['candidate_uid'], created = common.get_or_create_moyor_candidate_uid(c, candidate)
-    candidate['candidate_term_uid'] = '%s-%s' % (candidate['candidate_uid'], election_year)
-    upsertCandidates(candidate)
+c_another = conn_another.cursor()
+election_year = '2014'
+
+scope = ['https://spreadsheets.google.com/feeds']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('credential.json', scope)
+gc = gspread.authorize(credentials)
+sh = gc.open_by_key('1Cs5gEvSP7FdbS9vsQywUyaa-iVFk41e5hPk-GS53et0')
+worksheets = sh.worksheets()
+for wks in worksheets:
+    rows = wks.get_all_records()
+    position_type = 'mayors'
+    county = wks.title.replace(u'台', u'臺')
+    print county
+    for row in rows:
+        if not row[u'姓名']:
+            continue
+        candidate = {}
+        candidate['type'] = position_type
+        candidate['county'] = county
+        candidate['constituency'] = 0
+        candidate['name'] = common.normalize_person_name(row[u'姓名'])
+        candidate['election_year'] = election_year
+        candidate['birth'] = common.ROC2AD(row[u'出生年月日'])
+        candidate['party'] = common.normalize_party(row[u'政黨'])
+        candidate['gender'] = row[u'性別']
+        candidate['number'] = row[u'號次']
+        candidate['education'] = row[u'學歷']
+        candidate['experience'] = row[u'經歷']
+        candidate['platform'] = row[u'政見']
+        candidate['image'] = "%s/mayors/%s/%s_%04d.jpg" % (common.storage_domain(), candidate['election_year'], candidate['county'], candidate['number'])
+        if position_type == 'mayors':
+            candidate['candidate_uid'], created = common.get_or_create_moyor_candidate_uid(c, candidate)
+        else:
+            candidate['candidate_uid'], created = common.get_or_create_candidate_uid(c, candidate)
+        candidate['candidate_term_uid'] = '%s-%s' % (candidate['candidate_uid'], election_year)
+        candidate['councilor_uid'], created = common.get_or_create_councilor_uid(c, candidate, create=False)
+        candidate['councilor_term_id'] = common.getDetailIdFromUid(c, candidate['councilor_uid'], election_year, candidate['county'])
+        candidate['councilor_terms'] = common.councilor_terms(c, candidate) if created else None
+        if position_type == 'mayors':
+            candidate['mayor_uid'] = candidate['candidate_uid']
+            if candidate['mayor_uid']:
+                candidate['mayor_terms'] = common.mayor_terms(c, candidate)
+            candidate['legislator_uid'] = common.get_legislator_uid(c_another, candidate['name'])
+            candidate['legislator_data'] = common.get_legislator_data(c_another, candidate['legislator_uid'])
+            if candidate['legislator_uid']:
+                candidate['legislator_terms'] = common.legislator_terms(c_another, candidate)
+                candidate['legislator_candidate_info'] = common.get_elected_legislator_candidate_info(c_another, candidate)
+                if candidate['legislator_candidate_info']:
+                    candidate['birth'] = candidate['legislator_candidate_info']['birth']
+        upsertCandidates(candidate)
 conn.commit()
