3c4d8065a50d2f7cbc9c506b8c6e5a60a5b98d0e
thewayiam <twly.tw@gmail.com>
2018-07-27 23:00:15 +0800

    candidates names to json for search

---
+++ parser/candidates/name2json_for_search.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append('../')
+import codecs
+import os
+import json
+
+import db_settings
+
+
+conn = db_settings.con()
+c = conn.cursor()
+election_year = '2018'
+
+c.execute('''
+    SELECT json_agg(_.name)
+    FROM (
+    SELECT name
+    FROM candidates_terms
+    WHERE election_year = %s
+    ORDER BY name
+    ) _
+''', [election_year])
+rows = c.fetchone()
+f_out = '../../voter_guide/static/json/dest/candidates_names_%s.json' % election_year
+json.dump(rows[0], codecs.open(f_out, 'w', 'utf-8'), ensure_ascii=False)
