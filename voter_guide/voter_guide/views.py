1c07b4e4994293e3bdf35394b60e31182b092c5e
thewayiam <twly.tw@gmail.com>
2018-08-02 17:00:29 +0800

    search redirect with anchor

--- voter_guide/voter_guide/views.py
+++ voter_guide/voter_guide/views.py
@@ -18,9 +18,9 @@ def home(request):
         try:
             candidate = Terms.objects.get(election_year=election_year, name=request.GET['name'])
             if candidate.type == 'mayors':
-                return redirect(reverse('candidates:mayors', kwargs={'county': candidate.county})+u'?name=%s' % request.GET['name'])
+                return redirect(reverse('candidates:mayors', kwargs={'county': candidate.county})+u'?name=%s#%s' % (request.GET['name'], candidate.candidate_id))
             else:
-                return redirect(reverse('candidates:district', kwargs={'county': candidate.county, 'constituency': candidate.constituency})+u'?name=%s' % request.GET['name'])
+                return redirect(reverse('candidates:district', kwargs={'county': candidate.county, 'constituency': candidate.constituency})+u'?name=%s#%s' % (request.GET['name'], candidate.candidate_id))
         except:
             pass
     return render(request, 'home.html')
