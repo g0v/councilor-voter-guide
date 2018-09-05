bf77ba1f5278a8cbd89aebeac616531120b163ce
thewayiam <twly.tw@gmail.com>
2018-09-05 14:59:46 +0800

    update register candidate's type

--- parser/candidates/register.py
+++ parser/candidates/register.py
@@ -30,7 +30,7 @@ def upsertCandidates(candidate):
         VALUES (%(candidate_term_uid)s, %(candidate_uid)s, %(councilor_term_id)s, %(councilor_terms)s, %(election_year)s, %(number)s, %(name)s, %(party)s, %(constituency)s, %(county)s, %(type)s, %(occupy)s, %(status)s)
         ON CONFLICT (election_year, candidate_id)
         DO UPDATE
-        SET elected_councilor_id = %(councilor_term_id)s, councilor_terms = %(councilor_terms)s, number = %(number)s, name = %(name)s, party = %(party)s, constituency = %(constituency)s, county = %(county)s, occupy = %(occupy)s, status = %(status)s, image = COALESCE(candidates_terms.image, %(image)s)
+        SET elected_councilor_id = %(councilor_term_id)s, councilor_terms = %(councilor_terms)s, number = %(number)s, name = %(name)s, party = %(party)s, constituency = %(constituency)s, county = %(county)s, type = %(type)s, occupy = %(occupy)s, status = %(status)s, image = COALESCE(candidates_terms.image, %(image)s)
     ''', complement)
     terms = []
     for t in ['mayor', 'legislator', 'councilor']:
