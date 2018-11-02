c1c42e8f146b40b89b94a127588b9f538dcc5899
thewayiam <twly.tw@gmail.com>
2018-11-02 16:49:57 +0800

    kmcc suggestions ext fix

--- crawler/kmcc/suggestions.py
+++ crawler/kmcc/suggestions.py
@@ -35,7 +35,7 @@ class Spider(scrapy.Spider):
     def parse_download(self, response):
         item = response.meta['item']
         item['url'] = response.url
-        item['file_ext'] = response.headers['Content-Disposition'].split('.')[-1]
+        item['file_ext'] = response.headers['Content-Disposition'].split('.')[-1].strip('"')
         cmd = u'mkdir -p ../../data/kmcc/suggestions/ && wget --heade="User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" -q -O ../../data/kmcc/suggestions/{year}_{month_from}-{month_to}.{file_ext} "{url}"'.format(**item)
         subprocess.call(cmd, shell=True)
         yield item
